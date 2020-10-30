226930ee1ba4854946adf338dea3936d8e91be99
tab <tab>
2020-10-30 11:01:45 +0100

    .

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,6 +1,11 @@
 import { checkFetcher } from '@apollo/client';
 import React from 'react'
 import ReactMarkdown from 'react-markdown';
+
+import { useQuery } from '@apollo/client'
+import NoteUser from './NoteUser'
+import { IS_LOGGED_IN } from '../gql/query
+
 import {format} from 'date-fns';
 import styled from 'styled-components';
 
@@ -25,6 +30,11 @@ const UserActions = styled.div`
 `;
 
 const Note = ({ note }) => {
+
+    const { loading, error, data } = useQuery(IS_LOGGED_IN);
+    if(loading) return <p> Loading...</p>;
+    if(error) return <p>Sorry, error fetchin data</p>;
+
     return (
         <StyledNote>
             <MetaData>
@@ -32,17 +42,22 @@ const Note = ({ note }) => {
                     <img src={note.author.avatar}
                     alt="{note.author.username} avatar"
                     heigh="50px"
-                    />{' '}
+                    />
                 </MetaInfo>
 
                 <MetaInfo>
                     <em>by</em> {note.author.username} <br/>
-                    {format(note.createdAt,' Do MMM YYYY')}
-                
+                    {format(note.createdAt,' Do MMM YYYY')}                
                 </MetaInfo>
-                <UserActions>
-                    <em>Favorites:</em> {note.favoriteCount}
-                </UserActions>
+                { data.isLoggedIn ? (
+                    <UserActions>
+                    <NoteUser note={note}/>
+                    </UserActions> 
+                ) : (
+                    <UserActions>
+                        <em>Favorites:</em> {note.favoriteCount}
+                    </UserActions>
+                )}
             </MetaData>
             
             <ReactMarkdown source={note.content} />
